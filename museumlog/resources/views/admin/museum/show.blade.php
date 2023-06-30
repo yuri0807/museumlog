719701dedc29279fa8e2694f61745a7cc6f7bbb0
Yuri <yurio.1011@gmail.com>
2023-06-30 08:38:56 +0000

    中央揃えへの整え

--- museumlog/resources/views/admin/museum/show.blade.php
+++ museumlog/resources/views/admin/museum/show.blade.php
@@ -4,7 +4,6 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
                 <form action="{{ route('admin.museum.show') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
@@ -14,26 +13,32 @@
                         </ul>
                     @endif
                     
+                   <div class="d-flex justify-content-center">
                    <div class="mb-3">
                         <img src="{{ secure_asset('storage/image/' . $museum->image_path) }}" class="img-fluid" style="max-height: 400px;">
                    </div>
+                   </div>
 
+                   <div class="d-flex justify-content-center">
                    <div class="form-group row">
                    <div class="text-center">
                          <span class="font-weight-bold text-dark fs-10" style="font-size: 24px; white-space: nowrap; display: inline-block; text-align: center; width: 100%;">{{ $museum->title }}</span>
                    </div>
                    </div>
+                   </div>
 
+                   <div class="d-flex justify-content-center">
                    <div class="form-group row">
                    <div class="text-center">
                          <span class="font-weight-bold text-dark fs-10" style="font-size: 18px; white-space: nowrap; display: inline-block; text-align: center; width: 100%;">{{ $museum->body }}</span>
                    </div>
                    </div>
+                   </div>             
                    
                     <div class="form-group row">
                         <input type="hidden" name="id" value="{{ $museum->id }}">
                         @csrf
-                    
+                     <div class="d-flex justify-content-center">
                      <div class="btn-group">
                          <a href="{{ route('museum.index', ['id' => $museum->id]) }}" class="btn btn-light" style="background-color: white; color: gray; margin-right: 10px;">
                          もどる
@@ -44,6 +49,7 @@
                          <a href="{{ route('admin.museum.edit', ['id' => $museum->id]) }}" class="btn btn-light" style="background-color: white; color: gray;">
                          編集
                          </a>
+                      </div>
                       </div>
                       </div>
                         </div>
